4c2d4f12e8e0b217ed85c7a6b4751eac66181858
jiaozhe <946505120@qq.com>
2020-07-25 11:17:42 +0800

    修改了端口3001

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", 'text/html')
   res.end(fs.readFileSync("./index.html"))
-}).listen(3000, () => {
+}).listen(3001, () => {
   console.log("Server Running At #000")
 })
