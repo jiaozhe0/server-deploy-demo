51ebff72697c7580e59c92d150fd7dbe225c441a
jiaozhe <946505120@qq.com>
2020-07-31 14:38:36 +0800

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,11 +13,11 @@ module.exports = {
 
   deploy: {
     production: {
-      user: 'jiaozhe',
-      host: '81.70.44.252',
+      user: 'pc',
+      host: '39.100.244.161',
       ref: 'origin/master',
       repo: 'git@github.com:jiaozhe0/server-deploy-demo.git',
-      path: '/home/jiaozhe/http',
+      path: '/home/pc/test',
       'pre-deploy-local': '',
       'post-deploy': 'pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
--- index.js
+++ index.js
@@ -5,6 +5,6 @@ http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", 'text/html')
   res.end(fs.readFileSync("./index.html"))
-}).listen(3001, () => {
+}).listen(3000, () => {
   console.log("Server Running At #000")
 })
